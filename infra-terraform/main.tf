62c1a04f5a1206af8f838f9239006f788392d674
Marc Nöthen <marc.j.noethen@protonmail.com>
2026-06-17 12:03:42 +0200

    fix: use aks vm size with available quota

--- infra-terraform/main.tf
+++ infra-terraform/main.tf
@@ -79,7 +79,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name           = "system"
     node_count     = 1
-    vm_size        = "Standard_B2s_v2"
+    vm_size        = "Standard_D2s_v3"
     vnet_subnet_id = azurerm_subnet.aks.id
   }
 

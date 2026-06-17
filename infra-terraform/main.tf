4d8728ab20bff75d2e0dccecf586a8dd71304780
Marc Nöthen <marc.j.noethen@protonmail.com>
2026-06-17 11:53:44 +0200

    fix: use allowed aks vm size

--- infra-terraform/main.tf
+++ infra-terraform/main.tf
@@ -79,7 +79,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name           = "system"
     node_count     = 1
-    vm_size        = "Standard_B2s"
+    vm_size        = "Standard_B2s_v2"
     vnet_subnet_id = azurerm_subnet.aks.id
   }
 

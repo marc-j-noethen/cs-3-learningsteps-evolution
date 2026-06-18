be0c66b3ec190a1644b573956548c20d5322a252
Marc Nöthen <marc.j.noethen@protonmail.com>
2026-06-18 10:51:29 +0200

    feat: grant aks pull access to acr

--- infra-terraform/main.tf
+++ infra-terraform/main.tf
@@ -89,3 +89,9 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   tags = var.tags
 }
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+}
